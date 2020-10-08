0f57b532f9cd5c1c80937e7d810c3279f4df9d71
Goi Jia Jian <jiajiannn@gmail.com>
2020-10-08 20:18:20 +0800

    feat: write-doc and read-doc api endpoints

--- functions/index.js
+++ functions/index.js
@@ -15,8 +15,33 @@ app.get("/", (req, res) => {
   return res.status(200).send("Hello World!");
 });
 
-app.post("/write-doc", async (req, res) => {});
+app.post("/write-doc", async (req, res) => {
+  const { collection, documentId, documentValue } = req.body;
+  const docRef = db.collection(collection).doc(documentId);
+  try {
+    const r = await docRef.set(documentValue);
+    console.log("Write success", r);
+    return res.status(200).end();
+  } catch (e) {
+    console.log("Write failure", e);
+    return res.status(500).end();
+  }
+});
 
-app.get("/read-doc", async (req, res) => {});
+app.get("/read-doc", async (req, res) => {
+  const { collection, documentId } = req.query; // Read from url param
+  const docRef = db.collection(collection).doc(documentId);
+  try {
+    const r = await docRef.get();
+    console.log("Read success", r);
+    return res.status(200).send({
+      name: r.id,
+      value: r.dataa()
+    });
+  } catch (e) {
+    console.error("Read failure", e);
+    return res.status(500).end();
+  }
+});
 
 exports.api = functions.https.onRequest(app);
